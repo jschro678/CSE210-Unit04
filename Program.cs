020b72faddb17494cc1d8f460de7a846614395d5
FIRST_NAME LAST_NAME <jschro678@gmail.com>
2022-10-25 15:17:10 -0600

    created program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Unit03.Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Director director = new Director();
+            director.StartGame();
+        }
+    }
+}
